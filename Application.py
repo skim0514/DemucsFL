738a51b7bc9936e695e95d31ebb253211c8ec6a1
Sean Kim <seankim0129@gmail.com>
2023-04-22 00:14:30 -0400

    edits towards files

--- Application.py
+++ Application.py
@@ -1,5 +1,9 @@
 from tkinter import *
 from tkinter  import filedialog
+import SeparateAudio
+import torchaudio
+import os
+
 
 class Application(Frame):
     def say_hi(self):
@@ -13,8 +17,11 @@ class Application(Frame):
 
         self.QUIT.pack({"side": "left"})
 
-        self.button = Button(text="Open", command=self.openFile)
-        self.button.pack()
+        self.openFile = Button(text="Separate 1 File", command=self.openFile)
+        self.openFile.pack()
+
+        self.openDirectory = Button(text="Separate Directory", command=self.openDirectory)
+        self.openDirectory.pack()
 
         self.hi_there = Button(self)
         self.hi_there["text"] = "Hello",
@@ -24,9 +31,68 @@ class Application(Frame):
 
     def openFile(self):
         filepath = filedialog.askopenfilename(filetypes=[("Audio Types", ".mp3 .wav")])
-        file = open(filepath, 'r')
-        print(file.read())
-        file.close()
+
+
+        # file = open(filepath, 'rb')
+
+        pathObject = os.path.abspath(filepath)
+        print(pathObject)
+        head, tail = os.path.split(pathObject)
+
+        print(tail)
+
+        if "mp3" in tail:
+            waveform, sample_rate = torchaudio.load(filepath, format="mp3")
+        else:
+            waveform, sample_rate = torchaudio.load(filepath)
+
+        self.separate(filepath, sample_rate, tail, waveform)
+
+    def separate(self, filepath, sample_rate, tail, waveform):
+        separation = SeparateAudio.SeparateAudio(waveform, sample_rate)
+        audios = separation.runSeparation()
+        print("separation")
+        # Vocals Audio
+        vocals = audios["vocals"]
+        # torchaudio.save(filepath + )
+        # Instrumental Audio
+        instrumental = audios["other"] + audios["drums"] + audios["bass"]
+        dir = os.path.splitext(filepath)[0]
+        if not os.path.exists(dir):
+            os.mkdir(dir)
+        else:
+            dir = self.check_file(dir)
+            os.mkdir(dir)
+        torchaudio.save(os.path.join(dir, "vocals" + tail), vocals, sample_rate)
+        torchaudio.save(os.path.join(dir, "instr" + tail), instrumental, sample_rate)
+
+    def check_file(self, filePath):
+        if os.path.exists(filePath):
+            numb = 1
+            while True:
+                newPath = "{0}_{2}{1}".format(*os.path.splitext(filePath) + (numb,))
+                if os.path.exists(newPath):
+                    numb += 1
+                else:
+                    return newPath
+        return filePath
+
+    def openDirectory(self):
+        filepath = filedialog.askdirectory()
+
+        for file in os.listdir(filepath):
+            filename = os.fsdecode(file)
+            print(filename)
+            if filename.endswith(".mp3") or filename.endswith(".wav"):
+                filefull = os.path.join(filepath, filename)
+                if filename.endswith("mp3"):
+                    waveform, sample_rate = torchaudio.load(filefull)
+                else:
+                    waveform, sample_rate = torchaudio.load(filefull)
+                self.separate(filefull, sample_rate, filename, waveform)
+            else:
+                continue
+
 
     def __init__(self, master=None):
         Frame.__init__(self, master)
---
+++ SeparateAudio.py
@@ -0,0 +1,103 @@
+import torch
+import torchaudio
+from torchaudio.pipelines import HDEMUCS_HIGH_MUSDB_PLUS
+from torchaudio.transforms import Fade
+
+
+class SeparateAudio:
+    def __init__(self, waveform, sample_rate):
+        self.waveform = waveform
+        self.sample_rate = sample_rate
+
+        return
+
+    def runSeparation(self):
+        bundle = HDEMUCS_HIGH_MUSDB_PLUS
+
+        model = bundle.get_model()
+
+        device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+        model.to(device)
+
+        self.sample_rate = bundle.sample_rate
+
+        waveform = self.waveform.to(device)
+        mixture = waveform
+
+        # parameters
+        segment: int = 10
+        overlap = 0.1
+
+        print("Separating track")
+
+        ref = waveform.mean(0)
+        waveform = (waveform - ref.mean()) / ref.std()  # normalization
+
+        sources = self.separate_sources(
+            model,
+            waveform[None],
+            device=device,
+            segment=segment,
+            overlap=overlap,
+        )[0]
+        sources = sources * ref.std() + ref.mean()
+
+        sources_list = model.sources
+        sources = list(sources)
+
+        audios = dict(zip(sources_list, sources))
+
+        return audios
+
+
+    def separate_sources(
+            self,
+            model,
+            mix,
+            segment=10.,
+            overlap=0.1,
+            device=None,
+    ):
+        """
+        Apply model to a given mixture. Use fade, and add segments together in order to add model segment by segment.
+
+        Args:
+            segment (int): segment length in seconds
+            device (torch.device, str, or None): if provided, device on which to
+                execute the computation, otherwise `mix.device` is assumed.
+                When `device` is different from `mix.device`, only local computations will
+                be on `device`, while the entire tracks will be stored on `mix.device`.
+        """
+        if device is None:
+            device = mix.device
+        else:
+            device = torch.device(device)
+
+        batch, channels, length = mix.shape
+
+        chunk_len = int(self.sample_rate * segment * (1 + overlap))
+        start = 0
+        end = chunk_len
+        overlap_frames = overlap * self.sample_rate
+        fade = Fade(fade_in_len=0, fade_out_len=int(overlap_frames), fade_shape='linear')
+
+        final = torch.zeros(batch, len(model.sources), channels, length, device=device)
+
+        while start < length - overlap_frames:
+            chunk = mix[:, :, start:end]
+            with torch.no_grad():
+                out = model.forward(chunk)
+            out = fade(out)
+            final[:, :, :, start:end] += out
+            if start == 0:
+                fade.fade_in_len = int(overlap_frames)
+                start += int(chunk_len - overlap_frames)
+            else:
+                start += chunk_len
+            end += chunk_len
+            if end >= length:
+                fade.fade_out_len = 0
+        return final
+
+
